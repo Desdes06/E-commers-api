ee2040d832624a892d5d268303e0b68e6cdb004c
Hikoyu <azrianpintar@gmail.com>
2025-05-15 09:21:59 +0700

    Review:  ada dua jenis cara penggunaan controller

--- src/controller/usersController.js
+++ src/controller/usersController.js
@@ -1,7 +1,27 @@
+// untuk sekala kecil
 const getAllUsers = (req, res) => {
-    res.json({
-        message : 'GET all user success'
-    })
-}
+  res.json({
+    message: "GET all user success",
+  });
+};
 
-module.exports = { getAllUsers };
+// jika sekala besar biasanya menggunakan async
+const getOneUser = async (req, res) => {
+  try {
+    res.json({
+      status: "success",
+      message: "Get on user",
+      data: {
+        username: "Azrian<3",
+        email: "azrianawan@gmail.com",
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { getAllUsers, getOneUser };
